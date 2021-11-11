25376665a72fa7ad0c4e5f13c1fa86bff081f56c
ntoliverapp <n.toliverapplication@gmail.com>
2021-11-11 00:58:24 -0800

    expenses

---
+++ expenses.py
@@ -0,0 +1,8 @@
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+# sum = 0
+# for x in expenses:
+#     sum = sum + x
+# print("You spent $",sum, " on lunch this week.", sep='')
+
+weekly_lunch = sum(expenses)
+print("You spent $",weekly_lunch, " on lunch this week.", sep='')
