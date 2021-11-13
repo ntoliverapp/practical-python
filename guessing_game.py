3e33ed896c60bd126cdb9dfd750bc1d12370e089
ntoliverapp <n.toliverapplication@gmail.com>
2021-11-13 14:06:00 -0800

    completed guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -2,25 +2,30 @@
 
 import random
 computer_guess = random.randint(0,100)
-print(computer_guess)
+total_guess = []
 
 guess = int(input("guess the number between 1 and 100: "))
-
+total_guess.append(guess)
 # Once the user guesses, the computer will tell the user if their guess was too high or too low
 
 while computer_guess:
     if guess == computer_guess:
-        print("your guess is: ", guess, "\n", "computer's guess is: ", computer_guess, "\n")
+        print("your guess is: ", guess, "\n", "computer's guess is: ", computer_guess)
         print("thats correct!")
+        print(total_guess)
         break
     elif guess < computer_guess:
-        print("your guess is: ", guess, "\n", "computer's guess is: ", computer_guess, "\n")
+        print("your guess is: ", guess)
         print ("your guess is too low")
         guess = int(input("guess again: "))
-
+        total_guess.append(guess)
     else: 
-        print("your guess is: ", guess, "\n", "computer's guess is: ", computer_guess, "\n")
+        print("your guess is: ", guess)
         print ("your guess is too high")
         guess = int(input("guess again: "))
+        total_guess.append(guess)    
 
-# The game is over once the user guesses the computer’s number. When the game is over, the computer the total number of guesses the user made before getting the right answer.
+# The game is over once the user guesses the computer’s number. When the game is over, the computer returns the total number of guesses the user made before getting the right answer.
+total = len(total_guess)
+print("the total number of guesses were: ", total)
+
